c7a6ecccc2538d84bd2fe7ea69679eb13fd9da08
EmaSuriano <emanuel.suriano@gmail.com>
2018-10-20 11:52:52 +0200

    set upn apollo-client

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import ApolloClient from 'apollo-boost';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Query
+              query={gql`
+                {
+                  rates(currency: "USD") {
+                    currency
+                    rate
+                  }
+                }
+              `}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error :(</p>;
+
+                return data.rates.map(({ currency, rate }) => (
+                  <div key={currency}>
+                    <p>{`${currency}: ${rate}`}</p>
+                  </div>
+                ));
+              }}
+            </Query>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
